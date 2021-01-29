2a35480fac4726a7ca3582988b14fa1f9b14d2a9
Arunabh133 <77616252+Arunabh133@users.noreply.github.com>
2021-01-29 21:43:50 +0600

    Script

---
+++ GGJ 2021/Assets/Developers/Arunbh & Lucas/playervelocity.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playervelocity : MonoBehaviour
+{
+    public float runSpeed = 20.0f;
+
+
+    private void Update()
+    {
+        var input = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0);
+        Vector3 velocity = input.normalized * runSpeed;
+        transform.position += velocity * Time.deltaTime;
+
+    }
+
+}
