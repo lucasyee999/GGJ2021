e8949d5f3e363d67efcfb30ef901709158baf198
lucas <61584821+lucasyee999@users.noreply.github.com>
2021-01-30 00:16:03 +0800

    Refactored playervelocity class

--- GGJ 2021/Assets/Developers/Arunbh & Lucas/Assets/PlayerController.cs
+++ GGJ 2021/Assets/Developers/Arunbh & Lucas/Assets/PlayerController.cs
@@ -1,10 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class playervelocity : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     public float runSpeed = 20.0f;
+    private NavMeshAgent _agent;
+
+    private void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+        _agent.updateRotation = false;
+        _agent.updateUpAxis = false;
+    }
 
 
     private void Update()
