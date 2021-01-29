07a58ec921b0542b598305062ed3e2b752b58fbe
Arunabh133 <77616252+Arunabh133@users.noreply.github.com>
2021-01-29 22:08:41 +0600

    Assets

---
+++ GGJ 2021/Assets/Developers/Arunbh & Lucas/Assets/cameraclamp.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraclamp : MonoBehaviour
+{
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(Mathf.Clamp(0,-11.4f,8.4f), Mathf.Clamp(0, 1.5f,-7.9f),transform.position.z);
+
+
+    }
+}
